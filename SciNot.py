8353f1bd2261522b23c33e80ae37477905c75b06
Liam Ramirez-Guess <liamr61@educbe.ca>
2025-03-20 08:12:46 -0600

    Added initial files

---
+++ SciNot.py
@@ -0,0 +1,20 @@
+#============================================
+#   Written by Liam Ramirez-Guess
+#   Created on March 20th, 2025
+#   Tested on CASIO calculators
+#   Licenced under GNU General Public License v3.0   
+#============================================
+
+def to_scientific_notation(number, dec):
+    """
+    Converts a number into scientific notation.
+    :param number: The number to convert.
+    :return: A string representing the number in scientific notation.
+    """
+    #return "{:.3e}".format(number)
+    return f"{number:.{int(dec)}e}"
+
+
+num = float(input("Enter a number: "))
+dec = float(input("Enter the number of significant digits: ")) - 1
+print("Scientific notation:", to_scientific_notation(num, dec))
